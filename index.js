599da6d7e3b366f7945018034ce9fe0b393e2033
Enzo Molinari Reis <enzo_reis@estudante.sesisenai.org.br>
2024-08-01 11:22:58 -0300

    Criar e Buscar Fabricante Prontos

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const conn = require('./db/conn')
+const Fabricante = require('./model/Fabricante')
+const Produto = require('./model/Produto')
+
+const PORT = 3000
+const hostname = 'localhost'
+/* ---------------------------------------------------------- */
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+/* ---------------------------------------------------------- */
+
+app.get('/fabricante', async (req,res)=>{
+    const data = req.query
+    try{
+        const pesq = await Fabricante.findOne({where: {marca : data.marca}})
+        if(pesq === null){
+            res.status(404).json({message: 'Fabricante inexistente no banco de dados'})
+        }else if(pesq.marca == data.marca){
+            res.status(200).json(pesq.marca)
+        }
+    }catch(err){
+        res.status(500).json({message: 'Erro ao consultar fabricantes do sistema'}) 
+    }
+})
+
+app.post('/fabricante', async (req,res)=>{
+    const data = req.body
+    try{
+        const pesq = await Fabricante.create(data, {raw:true})
+        res.status(201).json(pesq)
+    }catch(err){
+        res.status(500).json({message: 'Erro ao adicionar fabricante ao sistema'})
+    }
+})
+
+app.get('/', (req,res)=>{
+    res.end('Servidor Ativo')
+})
+/* ---------------------------------------------------------- */
+conn.sync().then(()=>{
+    app.listen(PORT, hostname, ()=>{
+        console.log(`O Servidor está rodando em ${hostname}:${PORT}`)
+    })
+}).catch((err)=>{
+    console.error('Banco de Dados não Conectado',err)
+})
